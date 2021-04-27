1e008a0235013e884ad5678da95369ad052daf4c
chubbystrings <martinsokwor@hotmail.com>
2021-04-27 22:01:41 +0100

    day_9

---
+++ javascript/day_9/movesInSquared.js
@@ -0,0 +1,10 @@
+function vertMirror(strng) {
+    // Your code
+    return [strng].reverse().join('');
+}
+function horMirror(strng) {
+    // Your code
+}
+function oper(fct, s) {
+    // Your code
+}
